e8bd6934513e5eee04b6fb8a1bcab98e8a17a554
Ryan Holt <44439777+RyanJHolt@users.noreply.github.com>
2020-12-30 16:29:55 +0000

    Topic 10 - Session based Auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,6 +6,9 @@ import bodyParser from 'body-parser';
 import './db';
 import {loadUsers} from './seedData'
 import usersRouter from './api/users';
+import session from 'express-session';
+import authenticate from './authenticate';
+
 
 dotenv.config();
 
@@ -29,8 +32,13 @@ const port = process.env.PORT;
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(session({
+    secret: 'ilikecake',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use('/api/genres', genresRouter);
-app.use('/api/movies', moviesRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 app.use('/api/users', usersRouter);
 app.use(errHandler);
 
