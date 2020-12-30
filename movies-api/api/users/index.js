600b468c403e3e8cc99730f747e94cd18c3db873
Ryan Holt <44439777+RyanJHolt@users.noreply.github.com>
2020-12-30 15:36:41 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -4,19 +4,35 @@ import User from './userModel';
 const router = express.Router(); // eslint-disable-line
 
 // Get all users
-router.get('/', (req, res) => {
-    User.find().then(users =>  res.status(200).json(users));
+router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find( {username: user}).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
 });
 
 // register
-router.post('/', (req, res ,next) => {
-    User.create(req.body).then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = {username: req.params.userName};
+    if (newFavourite && newFavourite.id) {
+        User.find(query).then(
+            user => {
+                (user.favourites)?user.favourites.push(newFavourite):user.favourites =[newFavourite];
+                User.findOneAndUpdate(query, {favourites:user.favourites}, {
+                    new: true
+                }).then(user => res.status(201).send(user));
+            }
+        ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user")
+    }
 });
 
 // Update a user
 router.put('/:id',  (req, res) => {
     if (req.body._id) delete req.body._id;
-    User.update({
+    User.updateOne({
         _id: req.params.id,
     }, req.body, {
         upsert: false,
